cbf9e705a3e0119813649a044b5929ff125a180d
Caio Rodrigues <caiorodrigues@hotmail.com>
2019-05-10 22:25:48 -0300

    refactoring

--- src/components/CompareList/index.js
+++ src/components/CompareList/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 
-<<<<<<< HEAD
 import { Container, Repository } from './styles';
 
 const ComponentList = () => (
   <Container>
     <Repository>
       <header>
-        <img src="https://avatar3.githubusercontent.com/u/69631?v=4" alt="facebook" />
+        <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="facebook" />
         <strong>react</strong>
         <small>facebook</small>
       </header>
@@ -27,36 +26,8 @@ const ComponentList = () => (
       </ul>
     </Repository>
     <Repository>
-      <header>
-        <img src="https://avatar3.githubusercontent.com/u/69631?v=4" alt="facebook" />
-        <strong>react</strong>
-        <small>facebook</small>
-      </header>
-      <ul>
-        <li>
-          95.019 <small>stars</small>
-        </li>
-        <li>
-          95.019 <small>forks</small>
-        </li>
-        <li>
-          95.019 <small>issues</small>
-        </li>
-        <li>
-          3 days ago <small>last commit</small>
-        </li>
-      </ul>
-    </Repository>
-    <Repository>
-      <header>
-        <img src="https://avatar3.githubusercontent.com/u/69631?v=4" alt="facebook" />
-=======
-const CompareList = () => (
-  <div>
-    <div>
       <header>
         <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="facebook" />
->>>>>>> feature/lista-repositorios
         <strong>react</strong>
         <small>facebook</small>
       </header>
@@ -71,7 +42,27 @@ const CompareList = () => (
           95.019 <small>issues</small>
         </li>
         <li>
-<<<<<<< HEAD
+          3 days ago <small>last commit</small>
+        </li>
+      </ul>
+    </Repository>
+    <Repository>
+      <header>
+        <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="facebook" />
+        <strong>react</strong>
+        <small>facebook</small>
+      </header>
+      <ul>
+        <li>
+          95.019 <small>stars</small>
+        </li>
+        <li>
+          95.019 <small>forks</small>
+        </li>
+        <li>
+          95.019 <small>issues</small>
+        </li>
+        <li>
           3 days ago <small>last commit</small>
         </li>
       </ul>
@@ -80,13 +71,3 @@ const CompareList = () => (
 );
 
 export default ComponentList;
-=======
-          3 days ago<small>last commit</small>
-        </li>
-      </ul>
-    </div>
-  </div>
-);
-
-export default CompareList;
->>>>>>> feature/lista-repositorios
